44902596186d6cc18ed2da909409951e55cfbdcf
Dinesh Adabala <dinesh.adabala417@gmail.com>
2024-02-13 10:59:34 +0530

    hai from monday stash3 check

---
+++ src/Student3.java
@@ -0,0 +1,26 @@
+
+    private int add(int a , int b){
+        if (a>5 && b>5){
+            int total = a + b;
+    private int c;
+
+    public Student3(int a, int b, int c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+    private int add(int a,int b,int c){
+        if (a>5 && b>5 && c>5){
+            int total = a+b+c;
+            System.out.println(total);
+        }
+        public String getStudent(){
+                    System.out.println("Stash 1");
+                }
+        public String getStudent(){
+            System.out.println("stash2 check");
+        }
+                public String getStudent(){
+                    System.out.println("stash3 check");
+    }
+}
